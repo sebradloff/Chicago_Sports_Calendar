457df57fc537dcad802c3a7c5c1cea63d37d2cf3
Sebastian Radloff <sradloff23@gmail.com>
2014-09-16 07:54:33 -0500

    added ncaamb url

--- db/testing_api_calls.rb
+++ db/testing_api_calls.rb
@@ -1,9 +1,12 @@
 require 'net/http'
 require 'open-uri'
 #NFL season schedule
-# http://api.sportsdatallc.org/nfl-t1/2014/REG/schedule.xml?api_key=cukszefxneth7fzucvgvvmzj
+# url = "http://api.sportsdatallc.org/nfl-t1/2014/REG/schedule.xml?api_key=cukszefxneth7fzucvgvvmzj"
 
-uri = URI("http://api.sportsdatallc.org/nfl-t1/2014/REG/schedule.xml?api_key=cukszefxneth7fzucvgvvmzj")
+#NCAAMB season schedule
+# url = "http://api.sportsdatallc.org/ncaamb-t3/games/2013/REG/schedule.xml?api_key=6w462pcpbx5sujtwp89h7wkg"
+
+uri = URI(url)
 response = Net::HTTP.get_response(uri)
 
 p response.body
