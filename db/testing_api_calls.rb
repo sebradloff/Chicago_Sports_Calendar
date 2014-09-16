88762372828ed382751883cc993b4c614703a15b
Sebastian Radloff <sradloff23@gmail.com>
2014-09-16 08:31:20 -0500

    added nba url query to file

--- db/testing_api_calls.rb
+++ db/testing_api_calls.rb
@@ -5,12 +5,16 @@ require 'open-uri'
 
 # NCAAMB season schedule
 # season hasn't started yet
-# url = "http://api.sportsdatallc.org/ncaamb-t3/games/2014/REG/schedule.xml?api_key=6w462pcpbx5sujtwp89h7wkg"
+# probably just need to change the 2013 to 2014 in the url to get relevant info once available
+# url = "http://api.sportsdatallc.org/ncaamb-t3/games/2013/REG/schedule.xml?api_key=6w462pcpbx5sujtwp89h7wkg"
 
 # NHL season schedule
 # season hasn't started yet
 # url = "http://api.sportsdatallc.org/nhl-t3/games/2013/REG/schedule.xml?api_key=4qvmkkrb5wr7zpsvx694uxmf"
 
+# NBA season schedule
+# season hasn't started yet
+# url = "http://api.sportsdatallc.org/nba-t3/games/2013/REG/schedule.xml?api_key=g9vkejettza73je2zbvn2j3u"
 
 uri = URI(url)
 response = Net::HTTP.get_response(uri)
