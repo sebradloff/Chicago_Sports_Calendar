78c71d3fcacb6842bb48145944c24f862fe27836
Sebastian Radloff <sradloff23@gmail.com>
2014-09-16 08:25:29 -0500

    changed file roots to xml from rb and added NHL api call to test section

--- db/testing_api_calls.rb
+++ db/testing_api_calls.rb
@@ -1,10 +1,16 @@
 require 'net/http'
 require 'open-uri'
-#NFL season schedule
+# NFL season schedule
 # url = "http://api.sportsdatallc.org/nfl-t1/2014/REG/schedule.xml?api_key=cukszefxneth7fzucvgvvmzj"
 
-#NCAAMB season schedule
-# url = "http://api.sportsdatallc.org/ncaamb-t3/games/2013/REG/schedule.xml?api_key=6w462pcpbx5sujtwp89h7wkg"
+# NCAAMB season schedule
+# season hasn't started yet
+# url = "http://api.sportsdatallc.org/ncaamb-t3/games/2014/REG/schedule.xml?api_key=6w462pcpbx5sujtwp89h7wkg"
+
+# NHL season schedule
+# season hasn't started yet
+# url = "http://api.sportsdatallc.org/nhl-t3/games/2013/REG/schedule.xml?api_key=4qvmkkrb5wr7zpsvx694uxmf"
+
 
 uri = URI(url)
 response = Net::HTTP.get_response(uri)
