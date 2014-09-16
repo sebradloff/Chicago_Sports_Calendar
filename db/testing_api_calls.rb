9af33e42c08323779a056c5f32a6835999e6cfc6
Sebastian Radloff <sradloff23@gmail.com>
2014-09-16 07:44:36 -0500

    able to make api call and open response

--- db/testing_api_calls.rb
+++ db/testing_api_calls.rb
@@ -1,2 +1,9 @@
+require 'net/http'
+require 'open-uri'
+#NFL season schedule
+# http://api.sportsdatallc.org/nfl-t1/2014/REG/schedule.xml?api_key=cukszefxneth7fzucvgvvmzj
 
+uri = URI("http://api.sportsdatallc.org/nfl-t1/2014/REG/schedule.xml?api_key=cukszefxneth7fzucvgvvmzj")
+response = Net::HTTP.get_response(uri)
 
+p response.body
